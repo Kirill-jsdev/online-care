e8d1c2e565918700d779c8bbfa1b2997c6557882
Kirill Zlobin <jsdevzk@gmail.com>
2025-10-23 19:05:31 +0200

    Continued...

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -6,7 +6,7 @@ type LogoProps = {
 };
 
 const Logo = ({ width = 50, height = "auto", alt = "Zabota logo", className = "" }: LogoProps) => {
-  return <img src="/ZABOTA.svg" width={width} height={height as any} alt={alt} className={`block object-contain ${className}`} />;
+  return <img src="./ZABOTA.svg" width={width} height={height as any} alt={alt} className={`block object-contain ${className}`} />;
 };
 
 export default Logo;
